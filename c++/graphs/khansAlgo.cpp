dc27e649f5cc732c79121fae6b9e4a9c067dc26b
naveengitboi <naveenjangiti9@gmail.com>
2024-08-10 19:01:54 +0530

    kahns algorithm
    it wont use dfs and visited array

---
+++ c++/graphs/khansAlgo.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+
+vector<int> kahnsAlgo(vector<vector<int>> adj){
+  int n = adj.size();
+  vector<int> indegrees(n,0);
+  
+  vector<int> ans;
+  queue<int> que;
+  
+  for(int i =0; i<n;i++){
+    for(auto node: adj[i]){
+      indegrees[node] += 1;
+
+    }
+  }
+
+  for(int i =0; i< n; i++){
+    if(indegrees[i] == 0){
+      que.push(i);
+    }
+  }
+
+
+  while(que.empty()){
+    int x = que.front();
+    que.pop();
+    ans.push_back(x);
+    for(auto node: adj[x]){
+      indegrees[node] -= 1;
+      if(indegrees[node] == 0){
+        que.push(node);
+      }
+    }
+
+  }
+
+  return ans;
+
+}
