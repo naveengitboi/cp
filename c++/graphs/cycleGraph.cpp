c1fea03af773f159f3cc862ef3ba95f589324bc4
naveengitboi <naveenjangiti9@gmail.com>
2024-08-10 17:36:40 +0530

    template added

---
+++ c++/graphs/cycleGraph.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+class Node {
+public:
+  int data;
+  Node *next;
+  Node(int data, Node *next) : data(data), next(next){};
+  Node(int data) : data(data), next(nullptr){};
+};
+
+class solution {
+
+public:
+  bool dfs(int k, vector<int> *adj, vector<int> &path, vector<int> &visited) {
+    visited[k] = 1;
+    path[k] = 1;
+
+    for (auto each : adj[k]) {
+      if (!visited[each]) {
+        if (dfs(each, adj, path, visited)) {
+          return true;
+        }
+      } else if (path[k]) {
+        return true;
+      }
+    }
+
+    path[k] = 0;
+
+    return 0;
+  }
+  bool cycle(vector<int> *adj, int n) {
+
+    vector<int> path(n, 0), visited(n, 0);
+    for (int i = 0; i < n; i++) {
+      if (!visited[i]) {
+        if (dfs(i, adj, path, visited)) {
+          return true;
+        }
+      }
+    }
+
+    return 0;
+  }
+};
--- coding.cpp
+++ coding.cpp
@@ -1,51 +1,75 @@
 #include <bits/stdc++.h>
-#include <cmath>
 using namespace std;
 
-class Node{
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+/* clang-format on */
+
+/* Node for other platforms  */
+class Node {
 public:
   int data;
-  Node* next;
-  Node(int data, Node* next): data(data), next(next) {};
-  Node(int data) : data(data), next(nullptr) {};
+  Node *next;
+  Node(int data, Node *next) : data(data), next(next){};
+  Node(int data) : data(data), next(nullptr){};
 };
 
-// >n/2 problem moore voting algorithm
-class solution {
 
-public: 
-  bool dfs(int k, vector<int>* adj, vector<int>& path, vector<int>& visited)
-{
-    visited[k] = 1;
-      path[k] = 1;
 
-      for(auto each: adj[k]){
-        if(!visited[each]){
-          if(dfs(each, adj, path, visited)) {
-            return true;
-         }
-        }
-        else if(path[k]){
-        return true;
-        }
-    }
 
-    path[k]= 0;
-      
-    return 0;
-  }
-  bool cycle(vector<int>*adj, int n){
-    
-    vector<int> path(n, 0), visited(n, 0);
-    for(int i =0; i< n ; i++){
-      if(!visited[i]){
-        if(dfs(i, adj, path, visited)){
-          return true;
-        }
-      }
-    }
 
-    return 0;
-  }
 
-};
+
+
+
+/* Main()  function */
+int main() { 
+
+
+
+  return 0; 
+
+}
+/* Main() Ends Here */
