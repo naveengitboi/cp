d428954a18fa7837a8b192e76516b71aa19d2549
naveengitboi <naveenjangiti9@gmail.com>
2024-10-02 00:36:58 +0530

    moore voting n/3

--- codes/arrays/mooreVotingAlgo.cpp
+++ codes/arrays/mooreVotingAlgo.cpp
@@ -36,3 +36,50 @@ vector<int> approachOne(vector<int>& nums){
     }
     return ans;
 }
+
+
+//approach 2 
+
+
+vector<int> approachTwo(vector<int>& nums){
+    int leaderOne = INT_MIN;
+    int freq1 = 0;
+    int freq2 = 0;
+    int leaderTwo = INT_MIN;
+    int n = nums.size();
+    for(int i = 0; i< n;i++){
+        if(freq1 == 0){
+            leaderOne = nums[i];
+            freq1++;
+        }else if(nums[i] != leaderOne && freq2 == 0){
+            leaderTwo = nums[i];
+            freq2++;
+        }
+        else if(nums[i] == leaderOne){
+            freq1++;
+        }else if(nums[i] == leaderTwo){
+            freq2++;
+        }
+        else{
+            freq1--;
+            freq2--;
+        }
+    }
+    int cnt1 = 0, cnt2 = 0;
+    for(int i = 0; i<n; i++){
+        if(leaderOne == nums[i]){
+            cnt1++;
+        }
+        if(leaderTwo == nums[i]){
+            cnt2++;
+        }
+    }
+    vector<int> ans;
+    if(cnt1 > n/3){
+        ans.push_back(leaderOne);
+    }
+    if(cnt2 > n/3){
+        ans.push_back(leaderTwo);
+    }
+    return ans;
+}
