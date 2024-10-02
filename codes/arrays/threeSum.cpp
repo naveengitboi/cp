5727cb02da5392466ba8fe4c1eb20593cec47ba1
naveengitboi <naveenjangiti9@gmail.com>
2024-10-02 22:42:16 +0530

    three sum problem

--- codes/arrays/threeSum.cpp
+++ codes/arrays/threeSum.cpp
@@ -1,2 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define vvi vector<vector<int>>
+#define vi vector<int>
+
+
+vvi threeSumOptimal(vi& nums){
+    sort(nums.begin() , nums.end());
+
+    vvi ans;
+    int n = nums.size();
+    for(int i = 0; i< n; i++){
+        if(i > 0 && nums[i] == nums[i-1]){
+            continue;
+        }
+        int j = i+1;
+        int k = n-1;
+        while(j < k){
+            int target = - nums[i];
+            int sum = nums[j] + nums[i];
+            if(sum == k){
+                ans.push_back({nums[i], nums[j], nums[k]});
+                while(j+1< n && nums[j] == nums[j+1]) j++;
+                j++;
+                while(k > 0 && nums[k] == nums[k-1]) k--;
+                k--;
+            }
+            else if(sum < target){
+                k--;
+            }else{
+                j++;
+            }
+        }
+    }
+
+    return ans;
+}
+
