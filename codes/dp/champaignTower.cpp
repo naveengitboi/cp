e8d01fd777c60cbc25fb984c09dfa6c14cb25b12
naveengitboi <naveenjangiti9@gmail.com>
2024-10-12 13:01:02 +0530

    champagne problem using recursion and memo

--- codes/dp/champaignTower.cpp
+++ codes/dp/champaignTower.cpp
@@ -1,5 +1,38 @@
 // @leet start
 // champagneTower 799 leetcode question
+// @leet start
+// correct answer
+#include <bits/stdc++.h>
+#include <cstring>
+using namespace std;
+class SolveProblem {
+public:
+    double dp[101][101];
+    double solve(int poured, int i, int j){
+        if(i < 0 || j <0 || i< j) {
+            return 0.0;
+        }
+        if(i == 0&& j == 0){
+            return poured;
+        }
+        if(dp[i][j] != -1.0) return dp[i][j];
+        double leftUp = (solve(poured, i-1, j-1)-1)/2.0;
+        double rightUp = (solve(poured, i-1, j)-1)/2.0;
+        if(leftUp < 0) leftUp = 0.0;
+        if(rightUp < 0) rightUp = 0.0;
+        return dp[i][j] = leftUp + rightUp;
+    }
+    double champagneTower(int poured, int query_row, int query_glass) {
+        for(int i = 0 ;i <101; i++){
+            for(int j = 0; j< 101 ;j++){
+                dp[i][j] = -1.0;
+            }
+        }
+        double ans = solve(poured, query_row, query_glass);
+        return min(1.0, ans);
+    }
+};
+// @leet end
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
