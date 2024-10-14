c6134f685c20574c181890f741c49f5e960e6b28
naveengitboi <naveenjangiti9@gmail.com>
2024-10-15 01:32:12 +0530

    codign tle 2

--- coding.cpp
+++ coding.cpp
@@ -1,54 +1,58 @@
 #include <bits/stdc++.h>
 #include <climits>
-#include <sys/types.h>
 using namespace std;
 
-/* TYPES  */
+#define mod 1000000007
 #define ll long long
 #define pii pair<int, int>
-#define pll pair<long long, long long>
+#define vvi vector<vector<int>>
 #define vi vector<int>
-#define vll vector<long long>
+#define vs vector<string>
+#define vvs vector<vector<string>>
 #define mii map<int, int>
 #define si set<int>
-#define sc set<char>
-#define vvi vector<vector<int>>
-#define ull unsigned long long
+#define mll map<long long, long long>
 
-int solve(int arr[],int n, int k){
-    int prev = INT_MAX;
-    int zeros = 0;
-    for(int i = 0; i < n;i++){
+
+int solve(int arr[], int n , int k){
+    int cnt =0;
+    int prev = k-1;
+    for(int i = 0; i< n; i++){
         int rem = arr[i]%k;
+        if(arr[i]%2 == 0){
+            cnt++;
+        } 
         if(rem == 0){
-            if(k != 4) return 0;
-            zeros++;
+            return 0;
         }
-        prev  = min(prev, k - rem);
+        prev = min(prev, k-rem);
     }
     if(k == 4){
-        if(zeros < 1) return 2;
-        else if(zeros ==1) return 1;
-        else if(zeros > 1) return 0;
+        int val = INT_MAX;
+        if(cnt == 0){
+            val = 2;
+        }else if(cnt == 1) val=1;
+        else if(cnt > 1) val=0;
+        return min(val, prev);
     }
     return prev;
+
 }
-/* Main()  function */
-int main () {
+
+int main() {
     int t;
     cin>>t;
     while(t--){
         int n, k;
-        cin >> n >> k;
+        cin>>n>>k;
         int arr[n];
-        for(int i =0; i< n;i++){
+        for(int i = 0; i<n ; i++){
             cin>>arr[i];
         }
         int ans = solve(arr, n, k);
-        cout<< ans<<endl;
-
+        cout<<ans<<endl;
     }
-
     return 0;
 }
-/* Main() Ends Here */
+
+
