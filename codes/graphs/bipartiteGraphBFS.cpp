ab7b0ff04e043015f9ceb7944470af76fcf448fd
naveengitboi <naveenjangiti9@gmail.com>
2025-01-13 11:00:17 +0530

    BFS code

---
+++ codes/graphs/bipartiteGraphBFS.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long
+#define pii pair<int, int>
+#define vvi vector<vector<int>>
+#define vi vector<int>
+#define vs vector<string>
+#define vvs vector<vector<string>>
+#define mii map<int, int>
+#define si set<int>
+#define mll map<long long, long long>
+
+
+class Solution {
+public:
+    bool BFS(vii& adj, int u, vi& color, int currColor){
+        queue<int> q;
+        q.push(u);
+        color[u] = currColor;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto & v: adj[node]){
+                if(color[v] == color[node]) return false;
+                if(color[v] == -1){
+                    if(!BFS(adj, v, color, 1- currColor)) return false;
+                }
+            }
+        }
+        return true;
+  }
+    int checkBipartiteBFS(vii adj, int V) {
+        vi color(V, -1);
+
+        for(int i = 0; i< V; i++){
+            if(!BFS(adj, i, color, 1)) return false;
+        }
+        return true;
+    }
+};
+
