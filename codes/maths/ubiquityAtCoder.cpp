aa5585d51d4ad28934973f4f73d30f68ddfef6ae
naveengitboi <naveenjangiti9@gmail.com>
2026-06-09 01:25:46 +0530

    day 2

---
+++ codes/maths/ubiquityAtCoder.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+#define mod 1000000007
+#define int long long
+#define pii pair<int, int>
+#define vvi vector<vector<int>>
+#define vi vector<int>
+#define vs vector<string>
+#define vvs vector<vector<string>>
+#define mii map<int, int>
+#define si set<int>
+#define mll map<long long, long long>
+#define pb push_back
+#define f first
+#define s second
+#define b begin
+#define e end
+#define rb rbegin
+#define re rend
+#define gcd(a, b) __gcd(a,b)
+#define lcm(a,b) (a/(gcd(a, b)*b))
+#define bug(x) cout<<#x<<" "<<x<<endl
+
+// functions
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+bool isPrime(int n){
+    if(n < 2) return false;
+    for(int i =2; i <= round(sqrt(n)); i++){
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+void _sort(vi& a,bool rev=false){
+    if(rev){sort(a.rb(), a.re()); return;}
+    sort(a.b(), a.e()); return;
+}
+void _input(vi& a, int s, int  e){
+    for(int i = s; i <= e; i++){
+        cin>>a[i];
+    }
+}
+
+int powerMod(int x, int y){
+    int res = 1;
+    while(y--){
+        res = res*x%mod;
+    }
+    return res;
+}
+
+
+
+
+
+
+int solve(){
+    int n;
+    cin>>n;
+    if(n <= 1) return 0;
+    int ans = powerMod(10, n) - powerMod(9, n)*2 + powerMod(8, n);
+    ans %= mod;
+    ans = (ans + mod)%mod;
+    return ans;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout<<solve()<<endl;
+    return 0;
+}
+
+
